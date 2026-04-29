5c8eea5b6c72d1a0b3c67b22f5676449ad2b35d1
Roy Lin <roylin@a3s.dev>
2026-04-29 21:50:50 +0800

    chore: remove Python and Node SDKs from a3s-search

--- sdk/node/lib/index.ts
+++
@@ -1,9 +0,0 @@
-export { A3SSearch } from "./search";
-export { A3SSearchError, NativeModuleError, SearchError } from "./errors";
-export { ensureChrome } from "./native";
-export type {
-  SearchResult,
-  SearchOptions,
-  SearchResponse,
-  EngineError,
-} from "./types";
